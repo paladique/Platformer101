e8112edd862b54c61b7fa391e7778462dbefb4b2
Jasmine Greenaway <jasminegweb@gmail.com>
2017-02-22 10:22:04 -0500

    pickup a coin. just one

---
+++ Platformer101/Assets/Scripts/PickupCoin.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupCoin : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            Destroy(gameObject);
+    }
+}
